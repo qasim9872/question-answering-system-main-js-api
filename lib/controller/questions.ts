ebf5564b70281d58b304fdfae6a7ed77319b3773
Muhammad Qasim <Qasim9872@gmail.com>
2019-01-16 01:03:47 +0000

    endpoint to return all questions

---
+++ lib/controller/questions.ts
@@ -0,0 +1,13 @@
+import QuestionModel from "../model/question"
+
+export async function getQuestionsByParams(params: any) {
+  return QuestionModel.find({ ...params }).sort("-updatedAt")
+}
+
+export default async function getQuestions(id: string) {
+  const results = await (id
+    ? getQuestionsByParams({ _id: id })
+    : getQuestionsByParams({}))
+
+  return results.length === 1 ? results[0] : results
+}
