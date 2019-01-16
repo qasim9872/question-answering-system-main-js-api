a9fa83cb9be19bb9452e635055fe030da7824f5a
Muhammad Qasim <Qasim9872@gmail.com>
2019-01-16 01:54:27 +0000

    updating to filter files by extension when bootstrapping routes

--- lib/utils/api-setup.ts
+++ lib/utils/api-setup.ts
@@ -7,35 +7,48 @@ import Logger from "./logger"
 /* tslint:disable:no-var-requires */
 const validate = require("express-validation")
 
+const filterFileExtensions = [".ts", ".js"]
+
 export async function bootstrapRoutes(routes: string, logger: Logger) {
   const router = Router()
   const routeFileNames = await readdirAsync(routes)
 
   await Promise.all(
-    routeFileNames.map((file) => {
-      const filePath = join(routes, file)
-      import(filePath)
-        .then((routeImport) => {
-          const route: IRoute = routeImport.endpoint
-          const handlers = []
+    routeFileNames
+      .filter((file) => {
+        // filter type definitions by searching for '.d.' in the last 5 characters of the file name
+        if (file.includes(".d.", file.length - 5)) {
+          return false
+        }
 
-          const endpointPath: string = `/${route.name}`
+        const extension = file.substring(file.length - 3)
 
-          if (route.validationSchema) {
-            handlers.push(validate(route.validationSchema))
-          }
+        return filterFileExtensions.indexOf(extension) >= 0
+      })
+      .map((file) => {
+        const filePath = join(routes, file)
+        import(filePath)
+          .then((routeImport) => {
+            const route: IRoute = routeImport.endpoint
+            const handlers = []
 
-          handlers.push(route.handler)
+            const endpointPath: string = `/${route.name}`
 
-          router[route.requestType](endpointPath, handlers)
+            if (route.validationSchema) {
+              handlers.push(validate(route.validationSchema))
+            }
 
-          logger.info(`bootstrapped route: ${route.name}`)
-          return
-        })
-        .catch((err) => {
-          logger.error(`Error bootstrapping route:  ${file} ->  ${err}`)
-        })
-    })
+            handlers.push(route.handler)
+
+            router[route.requestType](endpointPath, handlers)
+
+            logger.info(`bootstrapped route: ${route.name}`)
+            return
+          })
+          .catch((err) => {
+            logger.error(`Error bootstrapping route:  ${file} ->  ${err}`)
+          })
+      })
   )
 
   return router
